5383cca95ae83b467236da2c32fdb55c39265e9e
Thanakrit Lee <contact@thanakritlee.dev>
2019-04-29 16:31:10 +0700

    Add test for CreateStudent and refactor code

--- chaincode/src/uniblock/uniblock_test.go
+++ chaincode/src/uniblock/uniblock_test.go
@@ -2,53 +2,67 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"testing"
 
 	"github.com/hyperledger/fabric/core/chaincode/shim"
+	"github.com/mitchellh/mapstructure"
+	"github.com/stretchr/testify/require"
 	"github.com/thanakritlee/dockerised-fabric-app/chaincode/src/uniblock/controllers"
 )
 
-func checkInit(t *testing.T, stub *shim.MockStub, args [][]byte) {
-
-	res := stub.MockInit("1", args)
-	if res.Status != shim.OK {
-		fmt.Println("Init failed: ", string(res.Message))
-		t.FailNow()
-	}
-
-}
-
-func checkInvoke(t *testing.T, stub *shim.MockStub, args [][]byte) {
+// invoke calls the chaincode function with the given arguments.
+func invoke(t *testing.T, stub *shim.MockStub, args [][]byte) interface{} {
 
 	res := stub.MockInvoke("1", args)
-	if res.Status != shim.OK {
-		fmt.Println("Invoke failed", string(res.Message))
-		t.FailNow()
-	}
+	require.Equal(t, int32(shim.OK), res.Status)
 
 	result := struct {
 		Status int         `json:"status"`
 		Data   interface{} `json:"data"`
 	}{}
 	err := json.Unmarshal(res.Payload, &result)
-	if err != nil {
-		fmt.Println(err)
-		t.FailNow()
-	}
+	require.Nil(t, err)
 
-	if result.Status != 200 {
-		fmt.Println("Error: ", result.Data)
-		t.FailNow()
-	}
+	return result.Data
 
 }
 
+// TestInit tests that the chaincode init function works.
 func TestInit(t *testing.T) {
 
 	chaincode := new(controllers.UniBlock)
 	stub := shim.NewMockStub("uniblock", chaincode)
 
-	checkInit(t, stub, [][]byte{[]byte("init")})
+	res := stub.MockInit("1", [][]byte{[]byte("init")})
+	require.Equal(t, int32(shim.OK), res.Status)
+
+}
+
+// TestCreateStudnet tests that a student can be created.
+func TestCreateStudent(t *testing.T) {
+
+	chaincode := new(controllers.UniBlock)
+	stub := shim.NewMockStub("uniblock", chaincode)
+
+	student := controllers.Student{
+		StudentID:  "0001",
+		FirstName:  "FIRSTNAME",
+		MiddleName: "MIDDLENAME",
+		LastName:   "LASTNAME",
+		Age:        69,
+	}
+
+	studentAsByte, err := json.Marshal(student)
+	require.Nil(t, err)
+
+	resp := invoke(t, stub, [][]byte{[]byte("CreateStudent"), studentAsByte})
+
+	respStudent := controllers.Student{}
+	err = mapstructure.Decode(resp, &respStudent)
+	require.Nil(t, err)
+
+	student.DocType = "Student"
+
+	require.Equal(t, student, respStudent)
 
 }
